1f9501848497556dd47df97ad1e972ac70548afa
徐剑鹏 <janpun@live.cn>
2017-08-18 08:02:50 +0800

    更新幻灯片
    幻灯片功能已完成，动画效果未完成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -26,6 +26,7 @@
     </div>
     <div class="index-right">
       <!--<slide-show :slides="slides" :inv="invTime"></slide-show>-->
+      <slide-show :slides="slides" :inv="invTime"></slide-show>
       <div class="index-board-list">
         <div
           class="index-board-item"
@@ -45,7 +46,11 @@
 </template>
 
 <script>
+  import slideShow from '../components/slideShow'
   export default {
+    components: {
+      slideShow
+    },
     created: function () {
       this.$http.get('api/getNewsList')
       .then(function (res) {
@@ -56,6 +61,29 @@
     },
     data () {
       return {
+        invTime: 2000,
+        slides: [
+          {
+            src: require('../assets/slideShow/pic1.jpg'),
+            title: 'xxx1',
+            href: 'detail/analysis'
+          },
+          {
+            src: require('../assets/slideShow/pic2.jpg'),
+            title: 'xxx2',
+            href: 'detail/count'
+          },
+          {
+            src: require('../assets/slideShow/pic3.jpg'),
+            title: 'xxx3',
+            href: 'http://xxx.xxx.com'
+          },
+          {
+            src: require('../assets/slideShow/pic4.jpg'),
+            title: 'xxx4',
+            href: 'detail/forecast'
+          }
+        ],
         productList: {
           pc: {
             title: 'PC产品',
